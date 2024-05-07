df7ebc913be5713c9722b9e0e875d128bdd274d1
Sergei Bykov <serega1988bykov@gmail.com>
2024-05-07 20:02:04 +0500

    add Model Master

--- nails/models.py
+++ nails/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 
 
+class Master(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя мастера')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='Slug',
+                            validators=[
+                                MinLengthValidator(5, message='Миннимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов'),
+                            ])
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', default=None, blank=True,
+                              null=True, verbose_name="Фото мастера")
+    about_master = models.TextField(blank=True, verbose_name='О мастере')
